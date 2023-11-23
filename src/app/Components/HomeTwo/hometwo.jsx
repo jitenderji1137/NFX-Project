091b5f28e787b1a22c01e4047f9eb262ff6456f8
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-23 20:19:43 +0530

    NFX

--- src/app/Components/HomeTwo/hometwo.jsx
+++ src/app/Components/HomeTwo/hometwo.jsx
@@ -1,7 +1,11 @@
+'use client'
 import Styles from "./hometwo.module.css"
+import { useEffect,useRef } from "react";
 import Link from "next/link"
 import Image from "next/image"
 export default function HomeTwo(){
+    const vidRef=useRef();
+    useEffect(() => { vidRef.current.play(); },[]);
     return(
         <>
         <div className={Styles.HomeTwo}>
@@ -9,7 +13,7 @@ export default function HomeTwo(){
                 <div>Enjoy on your TV</div>
                 <div>Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</div>
             </div>
-            <div><Image src="/TVFrame.png" width={1000} height={1000}/><video autoplay playsinline muted loop><source src="/TVFrameVideo.m4v" type="video/mp4"/></video></div>
+            <div><video ref={ vidRef } autoplay playsinline muted loop><source src="/TVFrameVideo.m4v" type="video/mp4"/></video><Image src="/TVFrame.png" width={1000} height={1000}/></div>
         </div>
         </>
     )
