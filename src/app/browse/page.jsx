6a175a788e67ca0786f69a4efcf00a0a185f5bca
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-26 18:22:34 +0530

    NFX

--- src/app/browse/page.jsx
+++ src/app/browse/page.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Components/NavBar/navbar"
 import BrowseOne from "./Components/BrowseOne/browseone"
 import Scroller from "./Components/Scoroller/scroller"
+import Tab from "./Components/Tab"
 export const metadata = {
     title: 'Browse On Netflix Mirror || Watch TV Shows Online, Watch Movies Online',
     description: 'Unlock the ultimate entertainment experience with Netflix Mirror, where every movie and web series is at your fingertips, completely free of charge. No more waiting for the latest releases, no more financial barriers, Netflix Mirror is your passport to endless entertainment, available anytime, anywhere.',
@@ -28,6 +29,7 @@ export default function Page(){
         <Navbar/>
         <BrowseOne/>
         <Scroller/>
+        <Tab/>
         </>
     )
 }
